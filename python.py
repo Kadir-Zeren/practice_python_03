de45a10858e57794abce3c26d80446936b74a11c
kadir-zeren <k4dzeren@gmail.com>
2024-10-23 10:00:08 +0300

    practice

--- python.py
+++ python.py
@@ -1,3 +1,104 @@
 print(11-7)
 print(4+11.0)
 print('11 - 7')
+
+print('3+5')
+print('abc')
+
+num1, num2 = 35, 20
+num3 = num1 - num2
+print(num3)
+
+r = 5
+pi = 3.14 
+area = pi * r ** 2
+print(area)
+
+r = 5
+pi = 3.14 
+area = pi * (r ** 2)
+print(area)
+
+print(11 % 2)
+
+print((4 * 5) / 2)
+
+print(2**3)
+print(3**2)
+a = 2
+b = 8
+print((a*b) ** 0.5)
+
+a = 5
+a = a + 1
+a += 1
+
+a = (1+1) ** (4 **(2/ 2 *2) / 2)
+print(a)
+
+b = -3 ** 2
+print(b)
+
+c = (-3) ** 2
+print(c)
+
+a= 3
+b = 4
+hipotenus = (a **2 + b**2)**0.5
+print(hipotenus)
+
+print(a,b,hipotenus, sep='----->')
+print(a, end="bitti")
+print(b)
+print(hipotenus)
+
+
+print('abc',555)
+
+text1 = "I bought"
+text2 = "kg. of apple this morning"
+amount = 6
+text3 = text1 + " " + str(amount) + " " + text2
+print(text1, amount, text2)
+print("I bought", 6, "kg. of apple this morning")
+
+print('C:\\north pole\noise_penguins.txt')
+print('-------')
+print('first','second','third', sep='\t')
+
+text = 'Bu\gun\ben\neden\taksiye\bindim'
+print(text)
+
+print(True and False)
+
+print(bool(False))
+print(bool(0))
+print(bool([]))
+print(bool(''))
+
+print(1 and 1)
+print(not False)
+
+print(True and True and True)
+
+print('string' and 5 and 6.2)
+
+print(5 and 0 and False and 'abcc')
+
+print(5 and 0 and False or False)
+
+print(2 and 3)
+print(1 and 0)
+
+print([] and "happy")
+
+print(2 or 3)
+
+print(2 or "hello")
+print([] or "happy")
+print(None or ())
+
+text = 'abcdef'
+text[0]
+text[1]
+text[5]
